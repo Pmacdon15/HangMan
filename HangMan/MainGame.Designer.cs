37c83ed6d273a6b16c06fc0148a5ee9a17e7d32d
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2024-09-23 13:11:38 -0600

    Initial commitment

---
+++ HangMan/MainGame.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace HangMan
+{
+    partial class MainGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label_Letter_1 = new Label();
+            label_Letter_2 = new Label();
+            label_Letter_3 = new Label();
+            label_Letter_4 = new Label();
+            label_Letter_5 = new Label();
+            textBox_Input_Char = new TextBox();
+            label1 = new Label();
+            button_Check_Letter = new Button();
+            SuspendLayout();
+            // 
+            // label_Letter_1
+            // 
+            label_Letter_1.AutoSize = true;
+            label_Letter_1.BorderStyle = BorderStyle.FixedSingle;
+            label_Letter_1.Location = new Point(48, 95);
+            label_Letter_1.Name = "label_Letter_1";
+            label_Letter_1.Size = new Size(70, 32);
+            label_Letter_1.TabIndex = 0;
+            label_Letter_1.Text = "label1";
+            // 
+            // label_Letter_2
+            // 
+            label_Letter_2.AutoSize = true;
+            label_Letter_2.BorderStyle = BorderStyle.FixedSingle;
+            label_Letter_2.Location = new Point(145, 95);
+            label_Letter_2.Name = "label_Letter_2";
+            label_Letter_2.Size = new Size(70, 32);
+            label_Letter_2.TabIndex = 1;
+            label_Letter_2.Text = "label2";
+            // 
+            // label_Letter_3
+            // 
+            label_Letter_3.AutoSize = true;
+            label_Letter_3.BorderStyle = BorderStyle.FixedSingle;
+            label_Letter_3.Location = new Point(241, 95);
+            label_Letter_3.Name = "label_Letter_3";
+            label_Letter_3.Size = new Size(70, 32);
+            label_Letter_3.TabIndex = 2;
+            label_Letter_3.Text = "label3";
+            // 
+            // label_Letter_4
+            // 
+            label_Letter_4.AutoSize = true;
+            label_Letter_4.BorderStyle = BorderStyle.FixedSingle;
+            label_Letter_4.Location = new Point(324, 95);
+            label_Letter_4.Name = "label_Letter_4";
+            label_Letter_4.Size = new Size(70, 32);
+            label_Letter_4.TabIndex = 3;
+            label_Letter_4.Text = "label4";
+            // 
+            // label_Letter_5
+            // 
+            label_Letter_5.AutoSize = true;
+            label_Letter_5.BorderStyle = BorderStyle.FixedSingle;
+            label_Letter_5.Location = new Point(418, 95);
+            label_Letter_5.Name = "label_Letter_5";
+            label_Letter_5.Size = new Size(70, 32);
+            label_Letter_5.TabIndex = 4;
+            label_Letter_5.Text = "label5";
+            // 
+            // textBox_Input_Char
+            // 
+            textBox_Input_Char.Location = new Point(60, 269);
+            textBox_Input_Char.Name = "textBox_Input_Char";
+            textBox_Input_Char.Size = new Size(42, 35);
+            textBox_Input_Char.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(58, 223);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 30);
+            label1.TabIndex = 6;
+            label1.Text = "Input Letter";
+            // 
+            // button_Check_Letter
+            // 
+            button_Check_Letter.Location = new Point(108, 269);
+            button_Check_Letter.Name = "button_Check_Letter";
+            button_Check_Letter.Size = new Size(131, 40);
+            button_Check_Letter.TabIndex = 7;
+            button_Check_Letter.Text = "Check Letter";
+            button_Check_Letter.UseVisualStyleBackColor = true;
+            button_Check_Letter.Click += button_Check_Letter_Click;
+            // 
+            // MainGame
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button_Check_Letter);
+            Controls.Add(label1);
+            Controls.Add(textBox_Input_Char);
+            Controls.Add(label_Letter_5);
+            Controls.Add(label_Letter_4);
+            Controls.Add(label_Letter_3);
+            Controls.Add(label_Letter_2);
+            Controls.Add(label_Letter_1);
+            Name = "MainGame";
+            Text = " ";
+            Load += MainGame_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label_Letter_1;
+        private Label label_Letter_2;
+        private Label label_Letter_3;
+        private Label label_Letter_4;
+        private Label label_Letter_5;
+        private TextBox textBox_Input_Char;
+        private Label label1;
+        private Button button_Check_Letter;
+    }
+}
